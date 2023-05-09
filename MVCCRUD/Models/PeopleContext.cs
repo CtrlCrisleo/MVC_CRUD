7e7f1382d507be1724bd811cd86e6627d01505a2
Cristhian <cristhian.lcortes@gmail.com>
2023-05-08 22:32:34 -0500

    Agregar archivos de proyecto.

---
+++ MVCCRUD/Models/PeopleContext.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCCRUD.Models;
+
+public partial class PeopleContext : DbContext
+{
+    public PeopleContext()
+    {
+    }
+
+    public PeopleContext(DbContextOptions<PeopleContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Person> People { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseMySql("name=MyDbContext", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.31-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Person>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("people");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.DateBirth).HasColumnName("date_birth");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .HasColumnName("email");
+            entity.Property(e => e.FirstName)
+                .HasMaxLength(100)
+                .HasColumnName("first_name");
+            entity.Property(e => e.Gender)
+                .HasMaxLength(1)
+                .HasColumnName("gender");
+            entity.Property(e => e.IdNumber).HasColumnName("id_number");
+            entity.Property(e => e.IdType)
+                .HasMaxLength(30)
+                .HasColumnName("id_type");
+            entity.Property(e => e.LastName)
+                .HasMaxLength(100)
+                .HasColumnName("last_name");
+            entity.Property(e => e.Phone)
+                .HasMaxLength(20)
+                .HasColumnName("phone");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ MVCCRUD/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MVCCRUD.Models;
+
+public partial class Person
+{
+    public int Id { get; set; }
+
+    public string IdType { get; set; } = null!;
+
+    public int IdNumber { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public DateOnly DateBirth { get; set; }
+
+    public string Gender { get; set; } = null!;
+
+    public string? Email { get; set; }
+
+    public string? Phone { get; set; }
+}
